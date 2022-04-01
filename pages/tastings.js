94713a16fca5b1325de6118db92db5bed29112ce
Andrew Becker <31748416+abecker93@users.noreply.github.com>
2022-04-01 14:35:41 -0400

    Create tastings.js placeholder

---
+++ pages/tastings.js
@@ -0,0 +1,10 @@
+// page for tastings information, right now just placeholder
+   
+import React from 'react';
+import Layout from '../components/utils/Layout'
+
+const Tastings = () => {
+  
+}
+
+export default Tastings
