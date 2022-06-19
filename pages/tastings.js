c29671d8403362194e5f4d334cf51d80f6fffd09
Andrew Becker <31748416+abecker93@users.noreply.github.com>
2022-06-19 13:31:16 -0400

    Alex, check the text before submitting to main!

--- pages/tastings.js
+++ pages/tastings.js
@@ -11,18 +11,22 @@ const Tastings = (props) => {
 
     return (
         <React.Fragment>
-            <Layout>
                 <div
                     className="text-lg xl:text-2xl 2xl:text-3xl text-marv pt-5 text-center"
                 >
-                    Drink some wine you dirty wine sluts!
+                    Interested in trying before you buy? Stop by one of these wine tastings to find out what you like best
+                </div>
+                <div
+                        className="font-normal text-lg text-black leading-8 pt-2"
+                >
+                Tastings and tours are not being offered on premises at this time. Once this changes, on-site tastings and vineyard tours will be offered by appointment only. Interested in a free tasting? Check out the options below!
                 </div>
                 <div
                     className="pt-5"
                 >
                     <img
-                        src="https://media.graphcms.com/output=format:jpg/URsR4QRUQ9ujFDYHZsoJ?_ga=2.251210438.190919827.1634691739-722458239.1630371733"
-                        alt="Montpelier Vineyards — group photo, picking grapes"
+                        src="https://media.graphassets.com/output=format:jpg/cgTBHUIQISYIcvSv2r3A?_ga=2.251210438.190919827.1634691739-722458239.1630371733"
+                        alt="A selection of wines and meads. Doesn't it look good?"
                         className="mx-auto h-96"
                     />
                 </div>
